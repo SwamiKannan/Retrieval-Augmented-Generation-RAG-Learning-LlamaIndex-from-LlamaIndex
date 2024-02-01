bf0c240d8e497799a79ad3ac696aaf34e507ba58
Swaminathan Kannan <65940566+SwamiKannan@users.noreply.github.com>
2024-02-01 12:31:00 +0530

    Update query.py
    Entire structure of the setup of the vectorstore

--- src/query.py
+++ src/query.py
@@ -1,2 +1,39 @@
 from llama_index.vector_stores import ChromaVectorStore
 import chromadb
+from llama_index.embeddings.openai import OpenAIEmbedding
+from llama_index.llms import OpenAI
+from llama_index.storage import StorageContext
+from llama_index import VectorStoreIndex
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Parameters
+MODEL = 'gpt-3.5-turbo'
+EMBEDDINGS = 'text-embedding-3-small'
+TEMPERATURE = 0
+
+# Create embeddings object
+embed_model = OpenAIEmbedding(model=EMBEDDINGS)
+embedding = OpenAIEmbedding()
+
+# load vectorstores
+db = chromadb.PersistentClient(path="./llamassist_chroma")
+chroma_collection = db.get_collection("llamaindex-assistant")
+vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
+
+# Setup llm
+llm = OpenAI(model=MODEL, temperature=TEMPERATURE)
+
+# Contexts
+storage_context = StorageContext.from_defaults(vector_store=vector_store)
+
+# setup index
+
+
+def get_index():
+    index = VectorStoreIndex.from_vector_store(
+        vector_store, storage_context=storage_context
+    )
+    print('Index setup')
+    return index
