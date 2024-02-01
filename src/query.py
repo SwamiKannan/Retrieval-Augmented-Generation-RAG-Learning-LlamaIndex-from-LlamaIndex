3f8b9b0fc1e301c812ef8e5ecd44f516238a382a
Swaminathan Kannan <65940566+SwamiKannan@users.noreply.github.com>
2024-02-01 15:16:45 +0530

    Update query.py
    updated with callbacks

--- src/query.py
+++ src/query.py
@@ -3,8 +3,9 @@ import chromadb
 from llama_index.embeddings.openai import OpenAIEmbedding
 from llama_index.llms import OpenAI
 from llama_index.storage import StorageContext
-from llama_index import VectorStoreIndex
+from llama_index import VectorStoreIndex, ServiceContext
 from dotenv import load_dotenv
+from llama_index.callbacks import LlamaDebugHandler, CallbackManager
 
 load_dotenv()
 
@@ -31,9 +32,29 @@ storage_context = StorageContext.from_defaults(vector_store=vector_store)
 # setup index
 
 
-def get_index():
-    index = VectorStoreIndex.from_vector_store(
-        vector_store, storage_context=storage_context
-    )
-    print('Index setup')
+def get_index(callback=False):
+    if callback:
+        debug_handler = LlamaDebugHandler(print_trace_on_end=True)
+        callback_manager = CallbackManager(handlers=[debug_handler])
+        service_context = ServiceContext.from_defaults(
+            callback_manager=callback_manager)
+        index = VectorStoreIndex.from_vector_store(
+            vector_store=vector_store, storage_context=storage_context, service_context=service_context)
+
+    else:
+        index = VectorStoreIndex.from_vector_store(
+            vector_store, storage_context=storage_context
+        )
+    print('Data loaded... !')
     return index
+
+
+# Testing to see if the data is loaded correctly
+if __name__ == "__main__":
+    index = get_index()
+    query_engine = index.as_query_engine()
+
+    question = "What is a LlamaIndex query engine?"
+    response = query_engine.query(question)
+    print(response)
+    query_engine = index.as_query_engine()
