bdf7685f4ce88ef542e9c0f9554fd91d95d867b7
Swaminathan Kannan <65940566+SwamiKannan@users.noreply.github.com>
2024-02-02 19:40:21 +0530

    Update app.py
    changed layout in streamlit
    added index and chat engine

--- src/app.py
+++ src/app.py
@@ -1,16 +1,20 @@
 import streamlit as st
 import os
+from llama_index.memory import ChatMemoryBuffer
+from query import get_index
 
 st.set_page_config(
-    page_title='Welcome to the Llama Index Assistant',
-    layout='wide',
-    page_icon=':llama:'
+    page_title='Welcome to your Llama Index Assistant',
+    layout='centered',
+    page_icon=':llama:',
+    initial_sidebar_state='auto',
+
 )
-style_head = "<style>h2 {text-align: center;} img {align: center;}</style>"
-with st.columns(3)[1]:
-    st.markdown(style_head, unsafe_allow_html=True)
-    st.image(os.path.join("images", "header.jpg"), width=500)
-    st.header("Welcome to your LlamaIndex Assistant")
+# style_head = "<style>h2 {text-align: center;} img {align: center;}</style>"
+# with st.columns(3)[1]:
+#     st.markdown(style_head, unsafe_allow_html=True)
+st.image(os.path.join("images", "app", "header.jpg"), width=600)
+st.header("Welcome to your LlamaIndex Assistant")
 
 st.text('Check refresh')
 with st.sidebar:
@@ -18,3 +22,14 @@ with st.sidebar:
         "Choose a data source",
         ("Langchain", "LlamaIndex")
     )
+
+# Create index and chat_engine
+memory = ChatMemoryBuffer.from_defaults(token_limit=1500)
+index = get_index()
+db = index.as_chat_engine(
+    chat_mode='context',
+    memory=memory,
+    prompt='You are a librarian chatbot helping users answer questions based on the LlamaIndex documentation and provide code examples from the documentation when required.',
+    verbose=True)
+
+st.session_state['chat_engine'] = db
